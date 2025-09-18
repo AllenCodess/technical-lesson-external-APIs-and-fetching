155b70af79f1a98ab1aa1970e8c3174c3edf15ac
AllenCodess <Aph296@protonmail.com>
2025-09-17 23:58:35 -0400

    Fetch and display astronauts from API

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // Define the function to display astronauts
 
+function displayAstronauts(data) {
+  const astronautList = document.getElementById("astronaut-list");
+
+  data.people.forEach((person) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = person.name;
+    astronautList.appendChild(listItem);
+  });
+}
+
+fetch("http://api.open-notify.org/astros.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    displayAstronauts(data);
+  })
+  .catch(function (error) {
+    console.error("Error fetching data:", error);
+  });
 // Function to display astronauts data
 // Hint: Create a function called displayAstronauts(data)
 // Hint: Select the <ul> element with the id "astronaut-list"
